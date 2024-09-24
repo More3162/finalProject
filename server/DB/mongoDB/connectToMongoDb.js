cec8fb3e39920ec6bca6342d1e489da8470597dc
mor3162 <more3031996@gmail.com>
2024-09-24 12:21:07 +0300

    DB fix

--- server/DB/mongoDB/connectToMongoDb.js
+++ server/DB/mongoDB/connectToMongoDb.js
@@ -1,9 +1,10 @@
+const chalk = require('chalk');
 const { mongoose } = require('mongoose');
 
 const connectToMongoDb = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:3030/restaurants');
-        console.log('Connected to MongoDB local');
+        await mongoose.connect('mongodb://127.0.0.1:27017/restaurants');
+        console.log(chalk.red('Connected to MongoDB local'));
     }
     catch (error) {
         console.log('Error connecting to MongoDB');
